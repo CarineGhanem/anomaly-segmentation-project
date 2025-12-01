32a8e0f3dfa1d1b74b894b590aa7a317498cd74a
KEVSER GUNAYDIN s338221 <kgun.stud@gmail.com>
2025-12-01 22:41:06 +0100

    added anomaly score calculations

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import os
-import cv2
 import glob
 import torch
 import random
@@ -58,6 +57,7 @@ def main():
     parser.add_argument('--num-workers', type=int, default=4)
     parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--cpu', action='store_true')
+    parser.add_argument('--anomaly_score', default='max_logit')  #other option is entropy
     args = parser.parse_args()
     anomaly_score_list = []
     ood_gts_list = []
@@ -75,7 +75,15 @@ def main():
     model = ERFNet(NUM_CLASSES)
 
     if (not args.cpu):
-        model = torch.nn.DataParallel(model).cuda()
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
+        print("Using device:", device)
+        model = model.to(device)
+
 
     def load_my_state_dict(model, state_dict):  #custom function to load model when not all dict elements
         own_state = model.state_dict()
@@ -96,11 +104,25 @@ def main():
     
     for path in glob.glob(os.path.expanduser(str(args.input[0]))):
         print(path)
-        images = input_transform((Image.open(path).convert('RGB'))).unsqueeze(0).float().cuda()
-        images = images.permute(0,3,1,2)
+        images = input_transform((Image.open(path).convert('RGB'))).unsqueeze(0).float().to(device)
+        #images = images.permute(0,3,1,2)
         with torch.no_grad():
             result = model(images)
-        anomaly_result = 1.0 - np.max(result.squeeze(0).data.cpu().numpy(), axis=0)            
+        if args.anomaly_score == 'entropy':
+            # compute softmax probabilities
+            probabilities = torch.nn.functional.softmax(result, dim=1)
+            # compute entropy
+            entropy = -torch.sum(probabilities * torch.log(probabilities + 1e-6), dim=1)
+            anomaly_result = entropy.squeeze(0).data.cpu().numpy()
+        
+        elif args.anomaly_score == 'msp':
+            probabilities = torch.nn.functional.softmax(result, dim=1)
+            anomaly_result = 1.0 - np.max(probabilities.squeeze(0).data.cpu().numpy(), axis=0)      
+        else:
+            # compute max logit score
+            max_logit, _ = torch.max(result, dim=1)
+            anomaly_result = -max_logit.squeeze(0).data.cpu().numpy()
+            
         pathGT = path.replace("images", "labels_masks")                
         if "RoadObsticle21" in pathGT:
            pathGT = pathGT.replace("webp", "png")
@@ -131,7 +153,8 @@ def main():
              ood_gts_list.append(ood_gts)
              anomaly_score_list.append(anomaly_result)
         del result, anomaly_result, ood_gts, mask
-        torch.cuda.empty_cache()
+        if device.type == 'cuda':
+            torch.cuda.empty_cache()
 
     file.write( "\n")
 
